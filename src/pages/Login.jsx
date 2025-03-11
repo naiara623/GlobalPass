1e544c65d759a25b9c233a6462e9ab2a3a2d1d3d
naiara623 <naiara_r_silva@estudante.sesisenai.org.br>
2025-03-11 17:25:10 -0300

    Tela de Login feita

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,13 +8,32 @@ function Login() {
         </div>
         <div className="azul-Login">
           <div className="roxo-Login">
-            <div className="rosa-Login"></div>
-              <div className="amarelo-Login"></div>
-            <div className="rosa-Login"></div>
+            <div className="rosa-Login">
+                <h1 className='titulo-login'>Login</h1>
+            </div>
+              <div className="amarelo-Login">
+                <div className="marron-login">
+                <label className='labelnome-login'>Nome Completo</label>
+              <input type="text" className='inputNome-login' placeholder='Ex: Maria Knupp' />
+
+              <label className='labelcpf-login'>CPF</label>
+              <input type="text" className='inputcpf-login' placeholder='000.000.000-00' />
+
+              <label className='labelemail-login'>Email</label>
+              <input type="text" className='inputemail-login' placeholder='mariaKnupp@gmail.com' />
+
+              <label className='labeltele-login'>Telefone</label>
+             <input type="text" className='inputtele-login' placeholder='(00) 0000-0000' />
+
+                </div>
+              </div>
+            <div className="rosa2-Login">
+              <button className='Entrar-login'><span>Entrar</span></button>
+            </div>
           </div>
         </div>
 
-        <div className="vermelha1-Login">
+        <div className="vermelha2-Login">
         <div className="azulClaro2-Login"></div>
         </div>
     </div>
