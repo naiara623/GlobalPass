71c601ca9f8bf51d871afff36073eb6a77d74951
naiara623 <naiara_r_silva@estudante.sesisenai.org.br>
2025-04-07 15:18:02 -0300

    Atualoizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
   import './App.css'
 
-  // import Inicio from './pages/Inicio'
-  // import Login from './pages/Login'
-  // import BoasVindas from './pages/BoasVindas'
-  // import Cadastro from './pages/Cadastro'
-// import TermosUso from './pages/TermosUso'
+
 
 function App() {
 
   return (
     <>
-{/* <TermosUso/> */}
-    {/* <Cadastro/> */}
-    {/* <BoasVindas/> */}
-    {/* <Login/>  */}
-    {/* <Inicio/> */}
+
 
     </>
   )
