1b34918c3a13d1b5cf25ca1f6f48ccbd23567f6f
LauraMelek2008 <lauramelek30@gmail.com>
2025-05-27 17:17:40 -0300

    atualização da tela de transporte

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,7 +3,7 @@ import BoasVindas from "../pages/BoasVindas.jsx";
 import Inicio from "../pages/Inicio.jsx";
 import Login from "../pages/Login.jsx";
 import Saude from "../pages/Saude.jsx";
-// import Transporte from "../pages/Transporte.jsx";
+import Transporte from "../pages/Transporte.jsx";
 import CadastroEscuro from "../pages/CadastroEscuro.jsx";
 import Perfil from "../pages/Perfil.jsx"
 import Educação from "../pages/Educação.jsx";
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     {path: "/", element: <BoasVindas/>},
     {path: "/login", element: <Login />},
      {path: "/saude", element: <Saude />},
-    // {path: "/transporte", element: <Transporte />},
+    {path: "/transporte", element: <Transporte />},
     {path: "/telainicial", element: <Inicio />},
     {path: "/cadastro", element: < CadastroEscuro />},
     {path: "/perfildeusuario", element: <Perfil/>},
