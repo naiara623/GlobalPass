97c45c3e06fdc62a82b486355ce1026a420e785b
kayllany04 <kayllany_lima@estudante.sesisenai.org.br>
2025-06-04 01:55:49 -0300

    tela de Postagem Atualizada

--- src/pages/TelaDePostagem.jsx
+++ src/pages/TelaDePostagem.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import './TelaDePostagem.css'
 
 function TelaDePostagem() {
   return (
-    <div>TelaDePostagem</div>
+    <div className='div-q-inglobaTudo-TLPostagem'>
+
+        <div className="din-navbar-TLPostagem">
+                <div className='div-arrow-TLPostagem' > </div>
+        </div>
+
+        <div className='ingloba-post-text-butão-TLpostagem'>
+
+            <div className='div-Q-ingloba-conteDa-post' >
+                <div className='div-vazia-TLpostagem' ></div>
+                <div className='conteine-DaPostagem-TLpostagem' > </div>
+                <div className="div-2vazia-TLpostagem"></div>
+            </div>
+
+
+            <div className='div-Q-ingloba-conteDa-legenButton' >
+                 <div className='conteine-dalegenda-TLpostagem' > </div>
+        
+                  <div className="conteine-butao"></div>
+            </div>
+
+           
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
   )
 }
 
